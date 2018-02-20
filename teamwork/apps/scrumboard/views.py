91d5a604ffe943c9ffa3150c93634cde37646c5d
Katerina Chinnappan <kachinna@ucsc.edu>
2018-02-19 23:58:38 -0800

    add to views.py

--- teamwork/apps/scrumboard/views.py
+++ teamwork/apps/scrumboard/views.py
@@ -1,5 +1,6 @@
 import json
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 from django.core import serializers
 from django.core.serializers import serialize
 from django.core.serializers.json import DjangoJSONEncoder
@@ -8,6 +9,12 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.core.serializers.json import DjangoJSONEncoder
 from django.shortcuts import get_object_or_404, redirect, render
+from teamwork.apps.courses.models import *
+from teamwork.apps.projects.models import *
+
+from teamwork.apps.core.models import *
+from teamwork.apps.courses.models import *
+from teamwork.apps.core.helpers import *
 
 
 # TODO Send the right scrum board here
@@ -48,3 +55,35 @@ def myscrum(request):
 
 def view_projects_scrum(request):
     return render(request, 'scrumboard/view_projects_scrum.html', {})
+
+
+def myscrumprojects(request, projects):
+    """
+    Private method that will be used for paginator once I figure out how to get it working.
+    """
+    page = request.GET.get('page')
+
+    # Populate with page name and title
+    page_name = "My Projectsss"
+    page_description = "Projects created by " + request.user.username
+    title = "My Projectsd"
+
+    #print("hello\n\n")
+
+    return render(request, 'scrumboard/myscrum.html', {'page_name': page_name,
+                                                           'page_description': page_description, 'title': title,
+                                                            'projects': projects})
+
+
+@login_required
+def view_projects(request):
+    """
+    Public method that takes a request, retrieves all Project objects from the model,
+    then calls _projects to render the request to template view_projects.html
+    """
+    my_projects = Project.get_my_projects(request.user)
+    print("helloworldsss\n\n")
+    print(my_projects)
+    print("\n\n")
+
+    return myscrumprojects(request, my_projects)
