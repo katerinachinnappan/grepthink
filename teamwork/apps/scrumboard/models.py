423c610d7d1dd76755587a39e68841bd1cb45a75
Katerina Chinnappan <kachinna@ucsc.edu>
2018-02-21 18:25:00 -0800

    get scrums for users, break up myscrum.html

--- teamwork/apps/scrumboard/models.py
+++ teamwork/apps/scrumboard/models.py
@@ -10,6 +10,28 @@ class Board(models.Model):
     members = models.ManyToManyField(
         User, related_name='users')
 
+    # The Meta class provides some extra information about the ScrumBoard model.
+    class Meta:
+        # Verbose name is the same as class name in this case.
+        verbose_name = "Scrum Boards"
+        # Multiple Project objects are referred to as Projects.
+        verbose_name_plural = "Scrum Boards"
+
+
+    @staticmethod
+    def get_my_scrums(user):
+        """
+        Gets a list of ScrumBoard objects. Used in views then passed to the template.
+        """
+        # #Gets membership and ownership object of current user
+        mem = list(user.users.all())
+
+        owner = list(user.owner.all())
+
+        scrums = list(set(mem + owner))
+
+        return scrums
+
 
 class Column(models.Model):
     tittle = models.CharField(max_length=100, default='')
